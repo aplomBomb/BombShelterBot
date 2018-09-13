8b3371af5c26085747cc04f3781c01b1b1e9873d
bomb <seizethesteez@gmail.com>
2018-09-13 11:25:21 -0400

    Initial commit

---
+++ bot.js
@@ -0,0 +1,20 @@
+var Discord = require('discord.io');
+
+var bot = new Discord.Client({
+    token: "NDg5NTI3MDU5NTI2NDUxMjIw.DnsD6Q.V8mfuNcxu5kVX9SVMDJRmPW91TE",
+    autorun: true
+});
+
+bot.on('ready', function() {
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
+});
+
+bot.on('message', function(user, userID, channelID, message, event) {
+    if (message === "ping") {
+        bot.sendMessage({
+            to: channelID,
+            message: "pong"
+        });
+    }
+});
+
