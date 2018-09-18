a34d69e7e6c0fc4239707906c077ea2498f04261
aplombomb <seizethesteez@gmail.com>
2018-09-18 09:45:09 -0400

    start blacklist file/array

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const config = require('config.json');
 const client = new Discord.Client();
+const fs = require("fs");
 
 
 client.on('ready', () => {
@@ -14,21 +15,42 @@ client.on('ready', () => {
       var guild = client.guilds.get(config.guildID);
       var dingDongMember = guild.roles.get(config.dingDongRole).members;
 
-
+      
       dingDongMember.forEach(member => {       
         member.removeRole(config.dingDongRole).then(setTimeout(helloDingDong, 1000)).catch(console.error);    
       });
       
        
-  function helloDingDong() {
+  async function helloDingDong() {
     var newDingDong = guild.members.random();
-    newDingDong.addRole(config.dingDongRole).then(console.log(newDingDong)).catch(console.error);
-    var dingDongChannel = guild.channels.get(config.speechChannel).send("**Congratulations** " + newDingDong + " **! You're DingDong of the Day! SPEECH! SPEECH! SPEECH!**");
+    try {
+      newDingDong.addRole(config.dingDongRole).then(console.log(newDingDong)).catch(console.error);
+      try {
+        var dingDongChannel = guild.channels.get(config.speechChannel).send("**Congratulations** " + newDingDong + " **! You're DingDong of the Day! SPEECH! SPEECH! SPEECH!**");
+      } catch (error) {
+        console.log(error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    
+    
+    
   };
 
                                    
       setTimeout(timeCheck, 86400000);
     } else {
+      try {
+        const blFile = fs.readFileSync(__dirname + "/blackList.json", function (error, data) {
+          console.log(error);
+          console.log(data);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+      
+      const blackList = [];
       console.log("It is not midnight");
       setTimeout(timeCheck, 3600000);
     }
